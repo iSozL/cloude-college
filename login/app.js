6858ce0b9ba6cdb90c84c2a43579c9fd1ed4a4e5
jello <2511841570@qq.com>
2019-11-13 08:17:31 +0800

    :sparkles: 解决跨域问题

--- login/app.js
+++ login/app.js
@@ -19,6 +19,15 @@ var app = express();
 app.use('/node_modules/',express.static('./node_modules/'));
 // 配置模板引擎和 body-parser 一定要在 app.use(router) 挂载路由之前
 // parse application/x-www-form-urlencoded
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 app.engine('html',require('express-art-template'));
 app.set('view engine', 'html')
 app.use(bodyParser.urlencoded({extended:false}));
